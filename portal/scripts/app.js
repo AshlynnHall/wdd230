3156203fe4210577cfeb15d893183ef4a43b15c5
Ashlynn Hall <hall.ash9@gmail.com>
2023-09-15 09:40:01 -0600

    initial commit

---
+++ portal/scripts/app.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector('h1');
+const copyright = document.querySelector('#copyright');
+const modification = document.querySelector('#modification')
+
+h1.textContent = `Ashlynn Jean Hall`;
+
+copyright.textContent = `©️ ${new Date().getFullYear()}`;
+modification.textContent = `Last Updated: ${(document.lastModified)}`;
