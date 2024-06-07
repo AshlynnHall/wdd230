39a4af4d00e24d505891b79c8a9bf2e240b02355
Ashlynn Hall <hall.ash9@gmail.com>
2024-06-07 14:16:04 -0600

    added dark mode

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,21 +1,37 @@
-let darkbutton = document.getElementById("dark-mode")
-darkbutton.addEventListener("click", () =>{    
-    darkbutton.classList.toggle("dark")
-    if (darkbutton.classList.contains("dark")){
-        document.documentElement.style.setProperty('white');        
-        document.documentElement.style.setProperty('black');        
-        document.documentElement.style.setProperty('#1f1f1d');    
-        document.documentElement.style.setProperty('#588157');        
-        document.documentElement.style.setProperty('#3A5A40');    
-        document.documentElement.style.setProperty('#1e2f21');                
-    }
-    else{
-        document.documentElement.style.setProperty('black');        
-        document.documentElement.style.setProperty('white');        
-        document.documentElement.style.setProperty('#DAD7CD');    
-        document.documentElement.style.setProperty('#A3B18A');        
-        document.documentElement.style.setProperty('#588157');    
-        document.documentElement.style.setProperty('#3A5A40');                
-    }
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
 
-})
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#1d201f";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#c3dfe0";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
+
+// let darkbutton = document.getElementById("dark-mode")
+// darkbutton.addEventListener("click", () =>{    
+//     darkbutton.classList.toggle("dark")
+//     if (darkbutton.classList.contains("dark")){
+//         document.documentElement.style.setProperty('white');        
+//         document.documentElement.style.setProperty('black');        
+//         document.documentElement.style.setProperty('#1f1f1d');    
+//         document.documentElement.style.setProperty('#588157');        
+//         document.documentElement.style.setProperty('#3A5A40');    
+//         document.documentElement.style.setProperty('#1e2f21');                
+//     }
+//     else{
+//         document.documentElement.style.setProperty('black');        
+//         document.documentElement.style.setProperty('white');        
+//         document.documentElement.style.setProperty('#DAD7CD');    
+//         document.documentElement.style.setProperty('#A3B18A');        
+//         document.documentElement.style.setProperty('#588157');    
+//         document.documentElement.style.setProperty('#3A5A40');                
+//     }
+
+// })
